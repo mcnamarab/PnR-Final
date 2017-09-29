f08a47285d083d4fa69baa0c15d62384afc55513
McNamaraB <mcnamarab@gilmour.org>
2017-09-29 14:19:46 -0400

    to_the_right

--- student.py
+++ student.py
@@ -57,6 +57,17 @@ class Piggy(pigo.Pigo):
         """executes a series of methods that add up to a compound dance"""
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
+        self.to_the_right()
+        #self.to_the_left()
+        #self.now_kick()
+        #self.cha_cha()
+        #self.walk_it_by_yourself()
+
+    def to_the_right(self):
+        """sub routine of dance method"""
+        self.encR(5)
+        for x in range(3):
+            self.encF(5)
 
     def nav(self):
         """auto pilots and attempts to maintain original heading"""
