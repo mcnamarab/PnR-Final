bc3228d0bb095e6b60a26c53ed28a357a167b11e
McNamaraB <mcnamarab@gilmour.org>
2017-10-20 14:36:09 -0400

    add full_detect

--- student.py
+++ student.py
@@ -178,7 +178,7 @@ class Piggy(pigo.Pigo):
         total_objects = 0
         for x in range(4):
             self.stop()
-            total_objects += self.wide_scan()
+            total_objects += self.obstacle_detect()
             self.stop()
             self.encL(18)
             self.stop()
