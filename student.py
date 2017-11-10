d7ff9ebea92e2723c9f7d1fea8835baec5dcbb6b
McNamaraB <mcnamarab@gilmour.org>
2017-11-10 14:47:23 -0500

    update rot_turn_nav

--- student.py
+++ student.py
@@ -227,7 +227,7 @@ class Piggy(pigo.Pigo):
                 while self.dist() < self.SAFE_STOP_DIST:
                     self.right_rot()
                 end = datetime.datetime.utcnow()
-                time.sleep(5)
+
 
             elapsed = (end - start).seconds  # calculates difference in time between start and end
             print(elapsed)  # for testing
